a3547bdf546066db640ca690cf56acac6f73f464
Padma Patil <padma@email.com>
2018-08-19 11:26:00 -0700

    This works properly

--- js/scripts.js
+++ js/scripts.js
@@ -1,65 +1,16 @@
-/*$(document).ready(function() {
-  $("form#formOne").submit(function(event) {
-    var items = ["input#one", "input#two", "input#three", "input#four"];
-
-    var outputItems= items.map(function(item) {
-      return item.val().toUpperCase();
-    });
-
-    ("ul#output").append("<li>" + outputItems + "</li>");
-    event.preventDefault();
-  });
-});*/
-
-
-
 $(document).ready(function() {
-  $("#formOne").submit(function(event){
+  $("form#formOne").submit(function(event) {
     event.preventDefault();
     var items = ["one", "two", "three", "four"];
-    var outputItems = items.map(function(item) {
+    var outputItems= items.map(function(item) {
       return $("input#"+item).val();
     });
-    var Upper = outputItems.map(function(item){
+
+    var capital = outputItems.map(function(item) {
       return item.toUpperCase();
     });
-    Upper.sort();
-    Upper.forEach(function(item) {
-      $("#ouput").append("<li>"+item+"</li>");
-    });
+    capital.sort();
+    capital.forEach(function(item){
+    $("#output").append("<li>" + item + "</li>");})
   });
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*$(document).ready(function() {
-  $("#formOne").submit(function(event) {
-
-    var items = ["one", "two", "three","four"]
-
-    answers.map(function(item){
-      var outputItems = $('input#' + item).val().toUpperCase();
-
-      $("ul#output").append("<li>" + userInput + "</li>");
-    });
-
-    $("#output").show();
-
-    event.preventDefault();
-  });
-});*/
